500063d1d667bee6148a5673193266a80fbbe1c2
Kyle Dawkins <kdawkins@fastmail.fm>
2012-09-22 08:25:17 -0700

    Fleshing out optional client-side JS.

--- share/javascript/prospero.js
+++ share/javascript/prospero.js
@@ -1,22 +1,27 @@
 // prospero core js
+// requires jQuery
 
 var prospero = (function() {
-    function Component( var nodeId ) {
-        this._nodeId = nodeId;
-        this._children = [];
-        this._childrenByNodeId = {};
-        return this;
-    }
-
     var _componentRegistry = {};
 
+    function Component( nodeId ) {
+        return this.init( nodeId );
+    }
+
     Component.prototype = {
-        nodeId:   function() { return this._nodeId },
+        init: function( nodeId ) {
+            this._nodeId = nodeId;
+            this._children = [];
+            this._childrenByNodeId = {};
+            _componentRegistry[ nodeId ] = this;
+            return this;
+        },
+
+        nodeId: function() { return this._nodeId },
 
         addChild: function( child ) {
             this._children.push( child );
             this._childrenByNodeId[ child.nodeId() ] = child;
-            this._componentRegistry[ child.nodeId() ] = child;
         },
 
         children: function() {
@@ -28,9 +33,76 @@ var prospero = (function() {
         },
     };
 
+    // Parent of all FormComponents
+    function FormComponent( nodeId ) {
+        Component.apply( this, arguments );
+        return this;
+    }
+
+    jQuery.extend( FormComponent.prototype, Component.prototype, {
+        init: function( nodeId ) {
+            Component.prototype.init.apply( this, arguments );
+            this._isRequired = false;
+            this._errorMessages = {};
+            this._element = jQuery( "#" + nodeId );
+            return this;
+        },
+
+        value:    function()  { return this._element.val() },
+        setValue: function(v) { this._element.val( v )     },
+        isRequired:    function()  { return this._isRequired },
+        setIsRequired: function(v) { this._isRequired = v    },
+
+        // this will be overridden
+        hasValidValues: function() {
+            var self = this;
+            if ( self.isRequired() && !self.value() ) {
+                return false;
+            }
+            return true;
+        },
+
+        requiredErrorMessage: function() {
+            return this.errorMessageForKey( "IS_REQUIRED" );
+        },
+
+        setRequiredErrorMessage: function( msg ) {
+            this.setErrorMessageForKey( msg, "IS_REQUIRED" );
+        },
+
+        errorMessageForKey: function(key) {
+            return this._errorMessages[ key ] || ( this.form && this.form.controller ? this.form.controller.errorMessageForKey( key ) :  key );
+        },
+
+        setErrorMessageForKey: function( msg, key ) {
+            this._errorMessages[ key ] = msg;
+        },
+
+        displayErrorMessage: function( msg ) {
+            jQuery('#' + this.nodeId() + "-error").html( msg ).show( 'normal' ).css( "error" );
+        },
+
+        displayErrorMessageForKey: function( key ) {
+            this.displayErrorMessage( this.errorMessageForKey( key ) );
+        },
+
+        indicateValidationFailure: function() {
+            var self = this;
+
+            // special case that should work with most components:
+            if ( self.isRequired() && !self.value() && self.requiredErrorMessage() ) {
+                self.displayErrorMessage( self.requiredErrorMessage() );
+            }
+        }
+
+    });
+
+
+    // exports
     return {
-        // export the component class
+        // export the component classes
         Component: Component,
+        FormComponent: FormComponent,
 
         // export a method to look up components in the hierarchy
         componentWithId: function( nodeId ) {
